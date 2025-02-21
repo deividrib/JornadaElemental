6589fdeee92bd211bf4dfd2ff6477080be558d48
deividrib <202011130033@ifba.edu.br>
2025-02-21 03:50:01 -0300

    chapter 1 funcional

--- JornadaElemental.c
+++ JornadaElemental.c
@@ -1,182 +1,231 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
 #include "hardware/i2c.h"
-#include "hardware/dma.h"
+#include "hardware/pwm.h"
 #include "hardware/pio.h"
-#include "hardware/interp.h"
-#include "hardware/timer.h"
-#include "hardware/watchdog.h"
 #include "hardware/clocks.h"
-#include "hardware/uart.h"
+#include "ssd1306.h"
+#include "font.h"
+#include "matriz_led_control.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+// -----------------------------
+// Configurações e definições
+// -----------------------------
+#define OLED_WIDTH      128
+#define OLED_HEIGHT     64
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+// Definição dos pinos para o LED RGB (ajuste conforme sua placa)
+#define LED_R_PIN       13
+#define LED_G_PIN       11
+#define LED_B_PIN       12
 
-// Data will be copied from src to dst
-const char src[] = "Hello, world! (from DMA)";
-char dst[count_of(src)];
+// Definição do pino para o BUZZER
+#define BUZZER_PIN      10
 
-#include "blink.pio.h"
+// Definição dos pinos para os botões
+#define BUTTON_ACCEPT   5   // Botão para aceitar a aventura
+#define BUTTON_DENY     6   // Botão para recusar a aventura
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
+// Pino de saída para a matriz LED 5x5
+#define MATRIX_OUT_PIN  7
 
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
+// -----------------------------
+// Variáveis globais e instâncias
+// -----------------------------
+ssd1306_t display;
 
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+// Estrutura para controle da matriz LED via PIO
+pio_t meu_pio = {
+    .pio = pio0,
+    .ok  = false,
+    .i   = 0,
+    // Valores padrão; serão alterados para definir a cor do glifo
+    .r   = 0.0,
+    .g   = 0.0,
+    .b   = 0.0,
+    .sm  = 0
+};
+
+// Definição de um "glifo" enigmático para a matriz LED 5x5
+double glifo[25] = {
+    0.0, 0.3, 0.0, 0.3, 0.0,
+    0.3, 0.0, 0.3, 0.0, 0.3,
+    0.0, 0.3, 0.3, 0.3, 0.0,
+    0.3, 0.0, 0.3, 0.0, 0.3,
+    0.0, 0.3, 0.0, 0.3, 0.0
+};
+
+// -----------------------------
+// Funções auxiliares
+// -----------------------------
+
+// Configura o LED RGB para emitir um suave tom azul
+void set_led_blue() {
+    gpio_put(LED_R_PIN, 0);
+    gpio_put(LED_G_PIN, 0);
+    gpio_put(LED_B_PIN, 1);
 }
 
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+// Função para tocar uma única nota no buzzer usando PWM
+void play_note(uint frequency, uint duration_ms) {
+    if (frequency == 0) {
+        sleep_ms(duration_ms);
+        return;
+    }
+    
+    gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+    uint channel = pwm_gpio_to_channel(BUZZER_PIN);
+    
+    float divider = 100.0f;
+    pwm_set_clkdiv(slice_num, divider);
+    
+    uint32_t wrap = (uint32_t)(125000000 / (divider * frequency)) - 1;
+    pwm_set_wrap(slice_num, wrap);
+    
+    pwm_set_chan_level(slice_num, channel, wrap / 2);
+    pwm_set_enabled(slice_num, true);
+    
+    printf("Tocando nota: %d Hz, wrap: %ld\n", frequency, wrap);
+    sleep_ms(duration_ms);
+    
+    pwm_set_enabled(slice_num, false);
 }
 
+// Função para tocar uma sequência de notas (musiquinha)
+void play_tune() {
+    printf("Iniciando a musiquinha...\n");
+    play_note(262, 500);  // C4 por 500 ms
+    sleep_ms(50);
+    play_note(294, 500);  // D4
+    sleep_ms(50);
+    play_note(330, 500);  // E4
+    sleep_ms(50);
+    play_note(262, 500);  // C4 novamente
+    printf("Musiquinha finalizada.\n");
+}
 
+// Função para exibir um frame no display OLED
+void display_frame(const char *lines[], uint8_t y_positions[], int num_lines) {
+    ssd1306_fill(&display, 0);
+    for (int i = 0; i < num_lines; i++) {
+        ssd1306_draw_string(&display, lines[i], 8, y_positions[i]);
+    }
+    ssd1306_send_data(&display);
+}
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
-
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-
-
-
-int main()
-{
+// -----------------------------
+// main()
+// -----------------------------
+int main() {
     stdio_init_all();
+    sleep_ms(2000);
+    printf("Capitulo 1: O Chamado do Destino\n");
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+    // Inicializa o I2C para o display OLED (pinos SDA = 14, SCL = 15)
+    i2c_init(i2c1, 100 * 1000);
+    gpio_set_function(14, GPIO_FUNC_I2C);
+    gpio_set_function(15, GPIO_FUNC_I2C);
+    gpio_pull_up(14);
+    gpio_pull_up(15);
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    ssd1306_init(&display, OLED_WIDTH, OLED_HEIGHT, false, 0x3C, i2c1);
+    ssd1306_config(&display);
+    ssd1306_fill(&display, 0);
+    ssd1306_send_data(&display);
 
-    // Get a free channel, panic() if there are none
-    int chan = dma_claim_unused_channel(true);
-    
-    // 8 bit transfers. Both read and write address increment after each
-    // transfer (each pointing to a location in src or dst respectively).
-    // No DREQ is selected, so the DMA transfers as fast as it can.
-    
-    dma_channel_config c = dma_channel_get_default_config(chan);
-    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-    channel_config_set_read_increment(&c, true);
-    channel_config_set_write_increment(&c, true);
-    
-    dma_channel_configure(
-        chan,          // Channel to be configured
-        &c,            // The configuration we just created
-        dst,           // The initial write address
-        src,           // The initial read address
-        count_of(src), // Number of transfers; in this case each is 1 byte.
-        true           // Start immediately.
-    );
-    
-    // We could choose to go and do something else whilst the DMA is doing its
-    // thing. In this case the processor has nothing else to do, so we just
-    // wait for the DMA to finish.
-    dma_channel_wait_for_finish_blocking(chan);
-    
-    // The DMA has now copied our text from the transmit buffer (src) to the
-    // receive buffer (dst), so we can print it out from there.
-    puts(dst);
+    init_pio_routine(&meu_pio, MATRIX_OUT_PIN);
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
 
-    // Interpolator example code
-    interp_config cfg = interp_default_config();
-    // Now use the various interpolator library functions for your use case
-    // e.g. interp_config_clamp(&cfg, true);
-    //      interp_config_shift(&cfg, 2);
-    // Then set the config 
-    interp_set_config(interp0, 0, &cfg);
-    // For examples of interpolator use see https://github.com/raspberrypi/pico-examples/tree/master/interp
+    gpio_init(BUZZER_PIN);
+    gpio_set_dir(BUZZER_PIN, GPIO_OUT);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+    gpio_init(BUTTON_ACCEPT);
+    gpio_set_dir(BUTTON_ACCEPT, GPIO_IN);
+    gpio_pull_up(BUTTON_ACCEPT);
 
-    // Watchdog example code
-    if (watchdog_caused_reboot()) {
-        printf("Rebooted by Watchdog!\n");
-        // Whatever action you may take if a watchdog caused a reboot
+    gpio_init(BUTTON_DENY);
+    gpio_set_dir(BUTTON_DENY, GPIO_IN);
+    gpio_pull_up(BUTTON_DENY);
+
+    // -----------------------------
+    // Exibe o Frame 1 no OLED (primeira parte da narrativa)
+    // -----------------------------
+    const char *frame1_lines[] = {
+        "O Chamado do",
+        "Destino",
+        "Em uma noite",
+        "calma,"
+    };
+    uint8_t frame1_y[] = {0, 10, 20, 30};
+    display_frame(frame1_lines, frame1_y, 4);
+    sleep_ms(7000);
+
+    // -----------------------------
+    // Exibe o Frame 2 no OLED (continuação da narrativa)
+    // -----------------------------
+    const char *frame2_lines[] = {
+        "uma visao",
+        "misteriosa",
+        "revela um",
+        "segredo antigo"
+    };
+    uint8_t frame2_y[] = {0, 10, 20, 30};
+    display_frame(frame2_lines, frame2_y, 4);
+    sleep_ms(7000);
+
+    // -----------------------------
+    // Exibe o Frame 3: Pergunta se aceita a aventura
+    // -----------------------------
+    const char *frame3_lines[] = {
+        "Aceita aventura?",
+        "Aceitar: Botao 5",
+        "Recusar: Botao 6"
+    };
+    uint8_t frame3_y[] = {10, 30, 40};
+    display_frame(frame3_lines, frame3_y, 3);
+
+    // Define novos valores de cor para o glifo – ex.: tom alaranjado
+    meu_pio.r = 1.0;   // Vermelho em máximo
+    meu_pio.g = 0.5;   // Verde com intensidade média
+    meu_pio.b = 0.0;   // Azul desligado
+
+    // Em vez de usar a função "desenho_pio", usamos "desenho_pio_rgb" para aplicar a cor
+    desenho_pio_rgb(glifo, &meu_pio);
+
+    set_led_blue();              // Mantém o LED RGB azul
+    play_tune();                 // Toca a musiquinha
+
+    printf("Aguardando resposta: Aceitar (botao 5) ou Recusar (botao 6)...\n");
+    while (gpio_get(BUTTON_ACCEPT) && gpio_get(BUTTON_DENY)) {
+        tight_loop_contents();
     }
-    
-    // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
-    // second arg is pause on debug which means the watchdog will pause when stepping through code
-    watchdog_enable(100, 1);
-    
-    // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
-    watchdog_update();
 
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    if (!gpio_get(BUTTON_ACCEPT)) {
+        ssd1306_fill(&display, 0);
+        ssd1306_draw_string(&display, "Chamado Aceito!", 0, 20);
+        ssd1306_draw_string(&display, "Iniciando aventura...", 0, 40);
+        ssd1306_send_data(&display);
+        printf("Chamado Aceito! Aventura iniciada.\n");
+    } else if (!gpio_get(BUTTON_DENY)) {
+        ssd1306_fill(&display, 0);
+        ssd1306_draw_string(&display, "Chamado Recusado!", 20, 20);
+        ssd1306_draw_string(&display, "Fim da jornada.", 20, 40);
+        ssd1306_send_data(&display);
+        printf("Chamado Recusado! Jornada encerrada.\n");
     }
+
+    while (1) {
+        tight_loop_contents();
+    }
+
+    return 0;
 }
